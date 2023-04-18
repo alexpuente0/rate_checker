83ff2406f07e4df13ee23d8ce03a9a6f63cb05fd
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2023-04-18 14:41:53 -0600

    Project initialization

---
+++ lib/rate_checker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rate_checker/version"
+
+module RateChecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rate_checker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RateChecker
+  VERSION = "0.1.0"
+end
